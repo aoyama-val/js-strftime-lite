e22ecef9541ba4b7e0f209fad293e552010f3b53
Aoyama, Shotaro <aoyama@val.co.jp>
2018-10-17 10:07:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# strftimeの簡易版
+
+```
+console.log(strftimeLite("%Y-%m-%d %H:%M:%S (%a)"));
+// => "2018-10-17 10:06:14 (水)"
+```
---
+++ strftimeLite.js
@@ -0,0 +1,15 @@
+/**
+ * strftimeの簡易版
+ */
+function strftimeLite(format, date) {
+  date = date || new Date();
+  var result = format;
+  result = result.replace(/%Y/g, date.getFullYear());
+  result = result.replace(/%m/g, ("0" + (date.getMonth() + 1)).slice(-2));
+  result = result.replace(/%d/g, ("0" + date.getDate()).slice(-2));
+  result = result.replace(/%H/g, ("0" + date.getHours()).slice(-2));
+  result = result.replace(/%M/g, ("0" + date.getMinutes()).slice(-2));
+  result = result.replace(/%S/g, ("0" + date.getSeconds()).slice(-2));
+  result = result.replace(/%a/g, [ "日", "月", "火", "水", "木", "金", "土" ][date.getDay()]);
+  return result;
+}
